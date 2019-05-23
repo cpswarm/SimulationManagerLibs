248a80d376e30e47bff04213d20aefc0d048fda7
Rui Zhao <rzhao@ismb.it>
2019-05-23 19:36:38 +0200

    add verbosity level

--- be.iminds.iot.ros.core/src/be/iminds/iot/ros/core/RosImpl.java
+++ be.iminds.iot.ros.core/src/be/iminds/iot/ros/core/RosImpl.java
@@ -126,7 +126,7 @@ public class RosImpl extends AbstractNodeMain implements Ros {
 		// start ROS core if required
 		boolean start = !rosCoreActive();
 		if(start){
-			System.out.println("\n start installed ROS master ?  "+ start);
+			System.out.println("\n start installed ROS system ?  "+ start);
 			boolean startNative = false;   // -------keep always running native ros core
 			String rosCoreNative = context.getProperty("ros.core.native");
 			if(rosCoreNative != null){
